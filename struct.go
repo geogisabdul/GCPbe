484817a05cdc535f17971e1150e59aea9002d25d
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-11-14 18:03:30 +0800

    main

--- struct.go
+++ struct.go
@@ -1,19 +1,59 @@
-package pasetobd
+package gcf
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	// "time"
-)
-
-type User struct {
-	ID           primitive.ObjectID 	`bson:"_id,omitempty" json:"_id,omitempty"`
-	Username string `json:"username" bson:"username"`
-	Password string `json:"password" bson:"password"`
-	// Email		 string             	`bson:"email,omitempty" json:"email,omitempty"`
+type GeometryPolygon struct {
+	Coordinates [][][]float64 `json:"coordinates" bson:"coordinates"`
+	Type        string        `json:"type" bson:"type"`
 }
 
-type Credential struct {
-	Status  bool   `json:"status" bson:"status"`
-	Token   string `json:"token,omitempty" bson:"token,omitempty"`
-	Message string `json:"message,omitempty" bson:"message,omitempty"`
+type GeometryLineString struct {
+	Coordinates [][]float64 `json:"coordinates" bson:"coordinates"`
+	Type        string      `json:"type" bson:"type"`
+}
+
+type GeometryPoint struct {
+	Coordinates []float64 `json:"coordinates" bson:"coordinates"`
+	Type        string    `json:"type" bson:"type"`
+}
+
+type GeoJsonLineString struct {
+	Type       string             `json:"type" bson:"type"`
+	Properties Properties         `json:"properties" bson:"properties"`
+	Geometry   GeometryLineString `json:"geometry" bson:"geometry"`
+}
+
+type GeoJsonPolygon struct {
+	Type       string          `json:"type" bson:"type"`
+	Properties Properties      `json:"properties" bson:"properties"`
+	Geometry   GeometryPolygon `json:"geometry" bson:"geometry"`
+}
+
+type Geometry struct {
+	Coordinates interface{} `json:"coordinates" bson:"coordinates"`
+	Type        string      `json:"type" bson:"type"`
+}
+type GeoJson struct {
+	Type       string     `json:"type" bson:"type"`
+	Properties Properties `json:"properties" bson:"properties"`
+	Geometry   Geometry   `json:"geometry" bson:"geometry"`
+}
+
+type Properties struct {
+	Name string `json:"name" bson:"name"`
+}
+
+type LonLatProperties struct {
+	Type        string    `json:"type" bson:"type"`
+	Name        string    `json:"name" bson:"name"`
+	Volume      string    `json:"volume" bson:"volume"`
+	Coordinates []float64 `json:"coordinates" bson:"coordinates, omitempty"`
+}
+
+type Credents struct {
+	Status  string `json:"status" bson:"status"`
+	Message string `json:"message" bson:"message"`
+}
+
+type RegisterStruct struct {
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
 }
